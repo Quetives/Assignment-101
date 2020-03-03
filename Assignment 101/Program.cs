decf9fe7a9a6b3ce9e84076366c433ef428518f9
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-02 19:44:33 -0800

    Add project files.

---
+++ Assignment 101/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            Console.WriteLine("true or false:");
+            bool dui = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && ticket < 3 && dui == false);
+            Console.ReadLine();
+        }
+    }
+}
